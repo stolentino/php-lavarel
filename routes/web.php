6b214e8efc1295f351e3605479bfbf71200a5111
stolentino <stotino2000@yahoo.com>
2023-12-25 23:46:39 -0800

    add controller and resource controller

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+Route::get('/', 'AppController@index');
 
 Route::get('/tracks', function () {
     return view('welcome');
