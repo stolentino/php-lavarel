328bd10118d2d846ab51b3e0fca4309bf17e223a
stolentino <stotino2000@yahoo.com>
2024-01-15 20:56:13 -0800

    deploying migrations

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Course;
 
 use Illuminate\Http\Request;
 
@@ -8,7 +9,7 @@ class AppController extends Controller
 {
     //
     public function index(){
-        return view('library');
+        return view('library', ['courses' => Course::all()]);
     }
     
 }
